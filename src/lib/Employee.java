03d7e468966d30c5997611c5222055b5326089f0
IrjiSyahrul <irjisyahrulchannel@gmail.com>
2025-04-18 14:09:00 +0700

    Melakukan refactoring pada fungsi setMonthlySalary agar tidak ada perulangan code dengan logika yang sama.

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -40,19 +40,20 @@ public class Employee {
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
 			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+			
 		}else if (grade == 2) {
 			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+		
 		}else if (grade == 3) {
 			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+		
+		}else {
+			System.err.println("Input grade tidak valid!");
+			monthlySalary = 0;
+			return;
+		}
+		if(isForeigner){
+			monthlySalary = (int) (monthlySalary * 1.5);
 		}
 	}
 	
